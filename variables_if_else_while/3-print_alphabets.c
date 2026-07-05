40a020180396be2a7c60d9e93d0368d8868a3186
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-07-05 08:58:23 +0000

    Add task 3: alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, and then in uppercase.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char low = 'a';
+	char up = 'A';
+
+	while (low <= 'z')
+	{
+		putchar(low);
+		low++;
+	}
+
+	while (up <= 'Z')
+	{
+		putchar(up);
+		up++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
