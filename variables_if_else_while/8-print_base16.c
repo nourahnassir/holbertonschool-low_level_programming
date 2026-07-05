50fddbf8997b1f0f4ad500181f6cdd7f2bfbd2a7
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-07-05 09:07:57 +0000

    Add task 8: print numbers of base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all numbers of base 16 in lowercase.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	char ch = 'a';
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+
+	while (ch <= 'f')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
