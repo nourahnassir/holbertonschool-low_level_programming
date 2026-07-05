d5c9760277b1da7f12a99bdb594601e1cc6f2231
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-07-05 09:02:12 +0000

    Add task 5: print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf("\n");
+
+	return (0);
+}
