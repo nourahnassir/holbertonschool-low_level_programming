318bd775495312df5300daccc3bb9a6fb4c3bf40
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-07-05 09:04:20 +0000

    Add task 6: print numbers using putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 using putchar.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
