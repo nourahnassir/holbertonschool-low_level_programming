fc9e0efa678649a17eeac2edb99c3120e83ae189
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-07-10 20:03:51 +0000

    Fix: ultimate line reduction for print_times_table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -8,44 +8,35 @@ void print_times_table(int n)
 {
 	int i, j, prod;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			prod = i * j;
+			if (j != 0)
 			{
-				prod = i * j;
-				if (j == 0)
-				{
-					_putchar(prod + '0');
-				}
-				else if (prod < 10)
-				{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+				if (prod < 10)
 					_putchar(' ');
+				if (prod < 100)
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(prod + '0');
-				}
-				else if (prod < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((prod / 10) + '0');
-					_putchar((prod % 10) + '0');
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10) % 10) + '0');
-					_putchar((prod % 10) + '0');
-				}
 			}
-			_putchar('\n');
+			if (prod >= 100)
+			{
+				_putchar((prod / 100) + '0');
+				_putchar(((prod / 10) % 10) + '0');
+			}
+			else if (prod >= 10)
+			{
+				_putchar((prod / 10) + '0');
+			}
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
 
