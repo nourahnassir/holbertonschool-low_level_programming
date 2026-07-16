d1705e83799a7dcbd38801000b3947ca01d7fb63
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-07-16 15:38:56 +0000

    Add simple-calculator files

---
+++ simple-calculator/README.md
@@ -0,0 +1,8 @@
+# C - Simple Calculator
+
+An interactive, terminal-based calculator program written in C.
+
+## Compile Command
+```bash
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 calculator.c -o calculator
+
---
+++ simple-calculator/calculator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the simple calculator program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int choice = -1;
+
+	while (choice != 0)
+	{
+		printf("Simple Calculator\n");
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
+		printf("Choice: ");
+
+		scanf("%d", &choice);
+
+		if (choice == 0)
+		{
+			printf("Bye!\n");
+		}
+	}
+
+	return (0);
+}
+
