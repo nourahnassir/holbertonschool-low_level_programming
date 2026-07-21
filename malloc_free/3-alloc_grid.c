f2b014db6162ba343d757073ac02d29feaf31246
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-07-21 07:16:41 +0000

    Fix multiple definition of free_grid

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,22 +2,6 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid
- * @grid: the grid to free
- * @height: height of the grid
- */
-void free_grid(int **grid, int height)
-{
-	int h;
-
-	if (grid == NULL)
-		return;
-	for (h = 0; h < height; h++)
-		free(grid[h]);
-	free(grid);
-}
-
-/**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers
  * @width: width of the grid
  * @height: height of the grid
