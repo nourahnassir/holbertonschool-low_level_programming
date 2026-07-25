a880ea8e998506b593f4ba92caa5c5938df8f7bb
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-07-25 16:50:43 +0000

    Fix: include stdlib.h for NULL definition

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,18 +3,28 @@
 
 /**
  * struct dog - A dog structure
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
  * Description: Defines a new type struct dog with name, age, and owner.
  */
-typedef struct dog
+struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
+ char *name;
+ float age;
+ char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
 
