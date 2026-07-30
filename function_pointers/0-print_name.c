336424ca4836af75ac09ebe9d4650d44c12645f6
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-07-30 08:56:17 +0000

    Task 0: Add print_name function using function pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name using a pointer to a function
+ * @name: string name to print
+ * @f: pointer to the printing function
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
+
