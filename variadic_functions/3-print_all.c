e77ea0677b5db7403adb0d18686cda218b6440f1
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-08-01 13:08:53 +0000

    Task 3: Refactor print_all using function pointers to respect the 2 if statements limit

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,6 +3,46 @@
 #include <stdarg.h>
 
 /**
+ * print_char - prints a char
+ * @args: list of arguments
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an integer
+ * @args: list of arguments
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float
+ * @args: list of arguments
+ */
+void print_float(va_list args)
+{
+	printf("%f", (float)va_arg(args, double));
+}
+
+/**
+ * print_string - prints a string
+ * @args: list of arguments
+ */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (!str)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
  * print_all - prints anything based on format
  * @format: list of types of arguments passed to the function
  *
@@ -12,8 +52,14 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0, j = 0;
-	char *str;
 	char *sep = "";
+	char types[] = {'c', 'i', 'f', 's'};
+	void (*funcs[])(va_list) = {
+		print_char,
+		print_int,
+		print_float,
+		print_string
+	};
 
 	va_start(args, format);
 	while (format && format[i])
@@ -21,22 +67,10 @@ void print_all(const char * const format, ...)
 		j = 0;
 		while (j < 4)
 		{
-			if (format[i] == "cifs"[j])
+			if (format[i] == types[j])
 			{
 				printf("%s", sep);
-				if (j == 0)
-					printf("%c", va_arg(args, int));
-				if (j == 1)
-					printf("%d", va_arg(args, int));
-				if (j == 2)
-					printf("%f", (float)va_arg(args, double));
-				if (j == 3)
-				{
-					str = va_arg(args, char *);
-					if (!str)
-						str = "(nil)";
-					printf("%s", str);
-				}
+				funcs[j](args);
 				sep = ", ";
 			}
 			j++;
