c67cb9724e65377340f132566cd14d7c4399ae1c
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-08-15 15:33:01 +0000

    Fix betty documentation style in 4-hash_table_get.c

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: Pointer to the hash table to look into
+ * @key: The key you are looking for
+ *
+ * Return: The value associated with the element, or NULL if key not found
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
