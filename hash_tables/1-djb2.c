1362ca62af04f61f7cd6b9b70c27e71d92c8cec2
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-08-15 15:28:16 +0000

    Fix betty doc style in 1-djb2.c

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm
+ * @str: string to hash
+ *
+ * Return: hashed value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
