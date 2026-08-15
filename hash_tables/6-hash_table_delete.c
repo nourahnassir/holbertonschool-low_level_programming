69b7fe1a1d0e72689fe9d5f75c2422aa0b2a6103
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-08-15 15:26:07 +0000

    Fix betty documentation style in 6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: Pointer to the hash table to delete
+ *
+ * Return: Nothing
+ */
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
