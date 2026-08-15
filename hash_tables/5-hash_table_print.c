8189f3994729dc6b4c705230ffc9697651df7011
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-08-15 15:34:45 +0000

    Fix betty documentation style in 5-hash_table_print.c

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: Pointer to the hash table to print
+ *
+ * Return: Nothing
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
