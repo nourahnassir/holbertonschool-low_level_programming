70c13b1e808a0caad7d9b8b911839d77b2c4d1f8
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-08-15 17:04:39 +0000

    Fix session signatures and memory allocations matching session.h

--- secure_data_handling/session.c
+++ secure_data_handling/session.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "session.h"
 
-session_t *session_create(const char *id, int data)
+session_t *session_create(const char *id, unsigned int uid, const unsigned char *data, size_t data_len)
 {
 	session_t *sess;
 
@@ -19,7 +19,26 @@ session_t *session_create(const char *id, int data)
 		free(sess);
 		return (NULL);
 	}
-	sess->data = data;
+	sess->uid = uid;
+
+	if (data && data_len > 0)
+	{
+		sess->data = malloc(data_len);
+		if (!sess->data)
+		{
+			free(sess->id);
+			free(sess);
+			return (NULL);
+		}
+		memcpy(sess->data, data, data_len);
+		sess->data_len = data_len;
+	}
+	else
+	{
+		sess->data = NULL;
+		sess->data_len = 0;
+	}
+
 	return (sess);
 }
 
@@ -33,14 +52,39 @@ void session_destroy(session_t *sess)
 		free(sess->id);
 		sess->id = NULL;
 	}
+	if (sess->data)
+	{
+		free(sess->data);
+		sess->data = NULL;
+	}
 	free(sess);
 }
 
-int session_update(session_t *sess, int data)
+int session_update(session_t *sess, const unsigned char *data, size_t data_len)
 {
+	unsigned char *new_data;
+
 	if (!sess)
 		return (0);
 
-	sess->data = data;
+	if (data && data_len > 0)
+	{
+		new_data = malloc(data_len);
+		if (!new_data)
+			return (0);
+		memcpy(new_data, data, data_len);
+		if (sess->data)
+			free(sess->data);
+		sess->data = new_data;
+		sess->data_len = data_len;
+	}
+	else
+	{
+		if (sess->data)
+			free(sess->data);
+		sess->data = NULL;
+		sess->data_len = 0;
+	}
+
 	return (1);
 }
