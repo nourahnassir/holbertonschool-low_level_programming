0b966924b0c6c344cf7eb04540bd345d5898e499
nourahnassir <nourahnassiralotaibi1999@gmail.com>
2026-08-19 03:21:25 +0000

    Fix Betty style warnings for Task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it
+ * to the POSIX standard output.
  * @filename: The name of the file to read.
- * @letters: The number of letters it should read and print.
+ * @letters: The number of letters it should read.
  *
- * Return: The actual number of letters it could read and print, or 0 on failure.
+ * Return: The actual number of letters, or 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
